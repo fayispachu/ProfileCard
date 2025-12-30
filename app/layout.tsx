62b64c8fc0cf5b183e5910768db83f648a17942b
Fayis <fayizpachu217@gmail.com>
2025-12-31 00:04:21 +0530

    analytic added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/next"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -64,6 +64,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           }}
         />
         {children}
+           <Analytics />
       </body>
     </html>
   );
